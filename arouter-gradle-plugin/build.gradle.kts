9a593a6e25bde06e00124d7a7586e64299d49606
JailedBird <113177053+JailedBird@users.noreply.github.com>
2023-05-25 17:25:20 +0800

    publish: upload plugins Plugin Portal

--- arouter-gradle-plugin/build.gradle.kts
+++ arouter-gradle-plugin/build.gradle.kts
@@ -1,15 +1,7 @@
 plugins {
     id("java-gradle-plugin")
     id("org.jetbrains.kotlin.jvm") version "1.6.10"
-}
-
-gradlePlugin {
-    plugins {
-        register("ArouterGradlePlugin") {
-            id = "cn.jailedbird.arouter.plugin"
-            implementationClass = "cn.jailedbird.arouter_gradle_plugin.ARouterPlugin"
-        }
-    }
+    id("com.gradle.plugin-publish") version "1.2.0"
 }
 
 repositories {
@@ -17,6 +9,48 @@ repositories {
     mavenCentral()
     gradlePluginPortal()
 }
+
+// The project version will be used as your plugin version when publishing.
+group = "cn.jailedbird.arouter"
+version = "1.0.0-beta01"
+
+pluginBundle {
+    website = "https://github.com/JailedBird/ArouterGradlePlugin"
+    vcsUrl = "https://github.com/JailedBird/ArouterGradlePlugin"
+    tags = listOf("Arouter", "AGP7", "Arouter plugin", "AGP8", "Transform", "Auto Register")
+}
+gradlePlugin {
+
+    plugins {
+        register("ArouterGradlePlugin") {
+            id = "cn.jailedbird.arouter.plugin"
+            implementationClass = "cn.jailedbird.arouter_gradle_plugin.ARouterPlugin"
+            displayName = "Arouter AGP7.4+ plugin"
+            description = "Arouter AGP7.4+ plugin"
+        }
+    }
+}
+
+/*gradlePlugin {
+    website.set("https://github.com/JailedBird/ArouterGradlePlugin")
+    vcsUrl.set("https://github.com/JailedBird/ArouterGradlePlugin")
+    plugins {
+        create("greetingsPlugin") {
+            id = "io.github.johndoe.greeting"
+            implementationClass = "io.github.johndoe.gradle.GreetingPlugin"
+            displayName = "Gradle Greeting plugin"
+            description = "Gradle plugin to say hello!"
+            tags.set(listOf("search", "tags", "for", "your", "hello", "plugin"))
+        }
+        create("goodbyePlugin") {
+            id = "io.github.johndoe.goodbye"
+            implementationClass = "io.github.johndoe.gradle.GoodbyePlugin"
+            displayName = "Gradle Goodbye plugin"
+            description = "Gradle plugin to say goodbye!"
+            tags.set(listOf("search", "tags", "for", "your", "goodbye", "plugin"))
+        }
+    }
+}*/
 
 dependencies {
     implementation(kotlin("stdlib"))
@@ -27,8 +61,3 @@ dependencies {
     compileOnly("org.ow2.asm:asm-commons:9.4")
     compileOnly("org.ow2.asm:asm-tree:9.4")
 }
-
-group = "cn.jailedbird.arouter.plugin"
-version = "1.0.0-beta01"
-
-apply(from = "../gradle/maven-publish.gradle")
