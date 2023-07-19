59c0a1d21c79144b721ae366edd711c4bc5ed5d0
JailedBird <113177053+JailedBird@users.noreply.github.com>
2023-07-19 20:13:35 +0800

    feat: update maven

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,11 +4,11 @@ pluginManagement {
     repositories {
         maven(url="https://maven.aliyun.com/repository/public/")
         maven(url="https://maven.aliyun.com/repository/google/")
-        maven(url="https://maven.aliyun.com/repository/gradle-plugin/")
         google()
         mavenCentral()
         gradlePluginPortal()
         maven(url = "https://jitpack.io")
+        maven(url="https://maven.aliyun.com/repository/gradle-plugin/")
     }
 }
 dependencyResolutionManagement {
